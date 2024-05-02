a47dfb340079f12fd8a4efbd9e0ea0ef54ed571a
mayssajebali <144730448+mayssajebali@users.noreply.github.com>
2024-05-02 23:00:06 +0100

    ajout du back

---
+++ MedPlusBack/src/test/java/com/medplus/MedPlusBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedPlusBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
